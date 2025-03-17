f582a7ad7a67b00d1d787f689e0fb2fb564e2443
Chris Ryan <117776119@umail.ucc.ie>
2025-03-17 10:10:32 +0000

    added trailinng pen and color change mechanics

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//load the 16 divs
 document.addEventListener("DOMContentLoaded", () => {   
 
     const container = document.querySelector('.container');
@@ -11,4 +12,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
         divCounter++;
     };
+
+    // add hover effect eventListener
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach((hoveredSquare) => {
+        //mouse enter event
+        hoveredSquare.addEventListener('mouseenter', () => {
+
+            hoveredSquare.classList.remove('square');
+            hoveredSquare.classList.add('hovered-square');
+        });
+        //mouse leave event
+        hoveredSquare.addEventListener('mouseleave', () => {
+        
+            hoveredSquare.classList.remove('hovered-square');
+            hoveredSquare.classList.add('square');
+        });
+        //drawing event
+        hoveredSquare.addEventListener('mousemove', (e) => {
+            
+            const drawDiv = document.createElement('div');
+            drawDiv.classList.add('pen-trail');
+
+            const x = e.clientX;
+            const y = e.clientY; //gets y position of mouse
+            drawDiv.style.left = `${x}px`;
+            drawDiv.style.top = `${y}px`;
+
+            hoveredSquare.appendChild(drawDiv);
+        });
+    });
 }); 
+
